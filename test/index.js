3bd18110c1a0daf967d693d3c45bd70c5e34bd34
Ethan James <necrozma@catgirlsaresexy.org>
2023-12-21 15:07:04 -0700

    Formatting

--- test/index.js
+++ test/index.js
@@ -3,7 +3,6 @@ const Rcon = require("../lib/index").Rcon;
 async function main() {
     try {
         const rconClient = new Rcon('10.0.0.150', 25575, 'test');
-
         await rconClient.connect();
         const response = await rconClient.send('list');
         console.log(response);
