33cfea5a49eac6abf4f84ce4d6c2bf54dd7fd68a
Ethan James <necrozma@catgirlsaresexy.org>
2023-12-21 14:53:20 -0700

    Added documentation

--- lib/index.ts
+++ lib/index.ts
@@ -1,25 +1,30 @@
 import net from 'net';
-var crypto = require('crypto');
-
-// RCON protocol constants
-const SERVERDATA_AUTH = 3;
-const SERVERDATA_EXECCOMMAND = 2;
-
-
-
-
+/**
+ * Represents a remote console (RCON) connection to interact with a server.
+ */
 export class Rcon {
+    // Private properties
     private socket: net.Socket;
     private id: number;
     private callbacks: Map<number, (packet: any) => void> = new Map();
-    
     private authenticated: boolean = false;
 
+    /**
+     * Creates an instance of Rcon.
+     * @param host - The host address of the server.
+     * @param port - The port number to connect to.
+     * @param password - The RCON password for authentication.
+     */
     constructor(private host: string, private port: number, private password: string) {
         this.socket = new net.Socket();
         this.id = this.randInt32();
     }
 
+    /**
+     * Establishes a connection to the RCON server.
+     * @returns A promise that resolves when the connection is established successfully.
+     * @throws Rejects with an error if the connection fails.
+     */
     public async connect() {
         return new Promise<void>((resolve, reject) => {
             this.socket.connect(this.port, this.host, () => {
@@ -31,6 +36,10 @@ export class Rcon {
             });
             this.socket.on('data', (data) => {
                 const packet = this.read(data);
+
+                // Check if the packet is a response to a command we sent
+                // If so, resolve the promise with the response
+
                 const callback = this.callbacks.get(packet.id);
                 if (callback) {
                     callback(packet);
@@ -40,6 +49,10 @@ export class Rcon {
         });
     }
 
+    /**
+     * Closes the connection to the RCON server.
+     * @returns A promise that resolves when the connection is closed.
+     */
     public async disconnect() {
         return new Promise<void>((resolve, reject) => {
             this.socket.end(() => {
@@ -48,21 +61,41 @@ export class Rcon {
         });
     }
 
+    /**
+     * Sends a command to the RCON server.
+     * @param command - The command string to be sent.
+     * @returns A promise that resolves with the response from the server.
+     * @throws Rejects with an error if authentication is required before sending commands.
+     */
     public async send(command: string) {
         return new Promise((resolve, reject) => {
+
+            // Check if we are authenticated before sending commands
             if (!this.authenticated) {
                 reject('Authentication required before sending commands.');
                 return;
             }
+
+            // Generate a unique ID for the command and add a callback to the map
             const id = this.id++;
             this.callbacks.set(id, (data) => {
                 resolve(data);
             });
-            const buffer = this.createBuffer(id, SERVERDATA_EXECCOMMAND, command);
+
+            // Create a buffer with the command and send it to the server
+            const buffer = this.createBuffer(id, 2, command);
             this.socket.write(buffer);
         });
     }
 
+    // Private methods
+
+    /**
+     * Performs authentication with the RCON server using the provided password.
+     * @param password - The RCON password for authentication.
+     * @returns A promise that resolves when authentication is successful.
+     * @throws Rejects with an error if authentication fails.
+     */
     private async auth(password: string) {
         return new Promise<void>((resolve, reject) => {
             const id = this.id++;
@@ -79,11 +112,20 @@ export class Rcon {
                     reject('Authentication failed: Unexpected response ID');
                 }
             });
-            const buffer = this.createBuffer(id, SERVERDATA_AUTH, password);
+
+            // Create a buffer with the password and send it to the server
+            const buffer = this.createBuffer(id, 3, password);
             this.socket.write(buffer);
         });
     }
 
+    /**
+     * Creates a buffer to be sent over the socket.
+     * @param id - The packet ID.
+     * @param type - The packet type.
+     * @param body - The content/body of the packet.
+     * @returns A buffer containing the formatted packet.
+     */
     private createBuffer(id: number, type: number, body: string) {
         const bodyBuffer = Buffer.from(body, 'utf8');
         const buffer = Buffer.alloc(14 + bodyBuffer.length);
@@ -96,6 +138,13 @@ export class Rcon {
         return buffer;
     }
 
+    /** 
+     * Reads a packet received from the server.
+     * @param packet - The packet buffer received from the server.
+     * @returns An object representing the parsed packet.
+     * @throws Error if the packet is invalid.
+     * @author github.com/tehbeard
+     */
     private read(packet: Buffer) {
         // Length of the rest of the packet
         const length = packet.readInt32LE(0);
@@ -113,10 +162,12 @@ export class Rcon {
         }
     }
 
+    /**
+     * Generates a random 32-bit integer for use as the initial packet ID.
+     * @returns A random 32-bit integer.
+     */
     private randInt32() {
         return Math.floor(Math.random() * 0xFFFFFFFF) | 0; // Use bitwise OR to limit to 32-bit range
     }
-    
-
 }
 
