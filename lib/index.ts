4d3780e86dae3e32adde93117a01afe2c3411520
Ethan James <necrozma@catgirlsaresexy.org>
2023-12-21 15:33:52 -0700

    Added License

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,21 @@
+/*
+    YAMRC establishes a connection to an RCON server, authenticates using a password, and sends commands to the server.
+    Copyright (C) 2023  Necrozma
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import net from 'net';
 /**
  * Represents a remote console (RCON) connection to interact with a server.
